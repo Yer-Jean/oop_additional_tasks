9010778bb12000d380eb4749319e2527e0fdd3dc
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-07-14 16:51:56 +0600

    Lesson 4 is done

--- lesson4/animal.py
+++ lesson4/animal.py
@@ -14,9 +14,26 @@
 """
 
 
-class Aminal:
-    pass
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
+    def speak(self):
+        print('?')
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def speak(self):
+        print('Woof!')
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def speak(self):
+        print('Meow!')
 
 animal = Animal("Animal")
 animal.speak()  # ?
